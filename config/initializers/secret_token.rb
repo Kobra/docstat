1a164903c2a93f723e1f44460d8c65df96644f96
Maxim Filatov <pipopolam@gmail.com>
2010-08-20 23:00:03 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Docstat::Application.config.secret_token = '006662ceef37a2cb85c8077423d69021a8e8804bccbd0eba3bed4eefb7d5dd5ac1cd07ffbaba4665ae0323c4f39d5f485de0def77ac1845e55e166b3bfa74756'
