3582b664a644d9208c3f1c39ab5825f7a49f7c04
Maxim Filatov <pipopolam@gmail.com>
2010-08-24 00:39:40 +0400

    Applied datapicker to #document_in_date

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+$(function() {$("#document_in_date").datepicker();});
+
 /* Google analytics template */
    var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']]; 
    (function(d, t) {
