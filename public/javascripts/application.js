c55848cb7a3e2998ecbe090660d9ec5b16de0443
Maxim Filatov <pipopolam@gmail.com>
2010-08-23 23:50:59 +0400

    DatePicker function for #document_in_date

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-/* Google analytics template */
-   var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']]; 
-   (function(d, t) {
+$(document).ready(function() {
+
+  /* Google analytics template */
+  var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+  (function(d, t) {
     var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
     g.async = true; g.src = '//www.google-analytics.com/ga.js'; s.parentNode.insertBefore(g, s);
-   })(document, 'script');
+  })(document, 'script');
+
+  $('#document_in_date').DatePicker({
+	  format:'m/d/Y',
+	  date: $('#document_in_date').val(),
+	  current: $('#document_in_date').val(),
+	  starts: 1,
+	  position: 'r',
+	  onBeforeShow: function(){
+		  $('#document_in_date').DatePickerSetDate($('#document_in_date').val(), true);
+	  },
+	  onChange: function(formated, dates){
+		  $('#document_in_date').val(formated);
+		  if ($('#closeOnSelect input').attr('checked')) {
+			  $('#document_in_date').DatePickerHide();
+		  }
+	  }
+  });
+});
