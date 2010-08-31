3ac307b3c9d3f180d7b5ec6923c04399af0629e2
Maxim Filatov <pipopolam@gmail.com>
2010-09-01 01:03:17 +0400

    $ rake rails:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,4 +22,7 @@ Docstat::Application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
 end
