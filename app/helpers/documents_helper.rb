1b5fcef3d236bca9c4ffa09843311867c843750c
Kobra Lopatich (l300) <lopatich@onmail.ru>
2010-09-05 03:17:58 +0400

    move form date localization from model to helper

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,5 @@
 module DocumentsHelper
+  def simple_form_l(value)
+    value ? I18n.l(value) : nil
+  end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -4,9 +4,4 @@ class Document < ActiveRecord::Base
   def status_name
     status.name if status
   end
-
-  def simple_form_datetime(attrname)
-    value = self.send(attrname)
-    value && DateTime === value ? I18n.l(value) : nil
-  end
 end
